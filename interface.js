25f5e5e1b4af51be6ac362f1092b122836b5acc0
Adam Lodowski <adamlodowski@gmail.com>
2019-01-22 16:55:21 +0000

    Added time and page now displays a single event

--- interface.js
+++ interface.js
@@ -1,9 +1,13 @@
 
 var submitEvent = document.querySelector(".Submitevent")
+var events = document.querySelector(".Events")
 
 submitEvent.onclick = () => {
   var listingDescription = document.querySelector(".listing_description").value 
   var listingDate = document.querySelector(".listing_date").value
-  console.log(new EventListing(listingDescription, listingDate))
-
+  var listingTime = document.querySelector(".listing_time").value
+  var newEvent = new EventListing(listingDescription, listingDate, listingTime)
+  events.innerHTML = `Description: ${newEvent.getListing()} <br> Date: ${newEvent.returnDate()} <br> Time: ${newEvent.returnTime()}` 
+  var eventAdded = document.querySelector("#EventAdded")
+  eventAdded.innerText = "Event added"
 }
--- src/eventListing.js
+++ src/eventListing.js
@@ -1,9 +1,10 @@
 'use strict'
 
 class EventListing {
-  constructor(listing, date){
+  constructor(listing, date, time){
     this.listing = listing
     this.date = date
+    this.time = time
   };
 
   getListing(){
@@ -13,7 +14,14 @@ class EventListing {
   returnDate(){
     return this.date
   }
+
+  returnTime(){
+    return this.time
+  }
+  
+  timeStamp(){
+    return this.returnDate.getDate()  + "-" + (this.returnDate.getMonth()+1) + "-" + this.returnDate.getFullYear() + " " +
+  this.returnDate.getHours() + ":" + this.returnDate.getMinutes();
+  }
+
 };
-
-
-
